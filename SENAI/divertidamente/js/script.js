50dd98c45cdeb776d71317dcf80124b5bed742b6
LeonardoBrito-XLBR <leonardobrito2707@gmail.com>
2024-08-27 22:38:36 -0300

    Baixar as imagens

--- SENAI/divertidamente/js/script.js
+++ SENAI/divertidamente/js/script.js
@@ -1,20 +1,31 @@
 // DOM
+//variavel recebe =  no documento HTML a busca ( class/id NOME)
 const imagem = document.querySelector("#foto")
 const btAlgeria = document.querySelector("#alegria")
 const btRaiva = document.querySelector("#raiva")
-const btTristeza = document.querySelector("#tristeza")
 const btNojinho = document.querySelector("#nojinho")
+const btTristeza = document.querySelector("#tristeza")
+const btMedo = document.querySelector("#medo")
+const btAnsiedade = document.querySelector("#ansiedade")
+const btInveja = document.querySelector("#inveja")
+const btTedio = document.querySelector("#tedio")
+const btVergonha = document.querySelector("#vergonha")
 const conteiner = document.querySelector('.opcao')
 
 //EVENTOS 
+//variavel quando clickado, criar a funcao
 btAlgeria.addEventListener("click", alegria)
 btRaiva.addEventListener("click", raiva)
 btTristeza.addEventListener("click", tristeza)
 btNojinho.addEventListener("click", nojinho)
+btAnsiedade.addEventListener("click", ansiedade)
+btInveja.addEventListener("click", inveja)
+btVergonha.addEventListener("click", vergonha)
+btTedio.addEventListener("click", tedio)
 
 //FUNÇAO 
 function alegria (){
-    imagem.src='images/02.png'
+    imagem.src='images/02.png' //substitui a imagem do html pela essa imagem
     // conteiner.style.backgroundColor = 'yellow'
 }
 
@@ -33,4 +44,9 @@ function nojinho (){
     imagem.src='images/03.png'
     conteiner.style.backgroundColor = 'green'
     
+}
+
+//BAIXAR E COLOCAR AS IMAGENS
+function ansiedade(){
+    imagem.src=''
 }
