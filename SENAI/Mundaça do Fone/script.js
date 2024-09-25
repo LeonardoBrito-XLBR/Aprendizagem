5739459947baef26b069b50de9d757a892fb7058
LeonardoBrito-XLBR <leonardobrito2707@gmail.com>
2024-09-24 22:19:08 -0300

    Concluido

--- SENAI/Mundaça do Fone/script.js
+++ SENAI/Mundaça do Fone/script.js
@@ -9,3 +9,16 @@ const opcao3 = document.querySelector("#botao3")
 opcao1.addEventListener('click', foto1)
 opcao2.addEventListener('click', foto2)
 opcao3.addEventListener('click', foto3)
+
+function foto1 (){
+    document.body.style.backgroundImage = "url('Images/01.jpg')";
+
+}
+
+function foto2 (){
+    document.body.style.backgroundImage = "url('Images/02.jpeg')";
+}
+
+function foto3 (){
+    document.body.style.backgroundImage = "url('Images/03.jpg')";
+}
